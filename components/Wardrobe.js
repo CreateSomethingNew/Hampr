1e52f64114d52e3314317b37a68f8d0b9d8d6ca9
Carson Robles <carson.robles@gmail.com>
2019-02-01 00:26:16 -0800

    Just a good put.

--- components/Wardrobe.js
+++ components/Wardrobe.js
@@ -3,6 +3,7 @@ import { Text, View } from 'react-native';
 
 class WardrobeScreen extends React.Component {
   render() {
+    console.log('TARK!');
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Wardrobe!</Text>
