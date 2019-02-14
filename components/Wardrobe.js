a98855866bad5fb6af90295bcabc42bc3f6a6754
Kartik Mendiratta <mendiratta.kartik@gmail.com>
2019-02-13 22:17:07 -0800

    added grid view to wardrobe screen and added more actions button

--- components/Wardrobe.js
+++ components/Wardrobe.js
@@ -1,43 +1,88 @@
 import React from 'react';
-import { Text, View, Button, TouchableHighlight, StyleSheet, Alert } from 'react-native';
+import { Text, View, Button, TouchableWithoutFeedback,
+         StyleSheet, Alert, FlatList, Image } from 'react-native';
 import { Header, Icon } from 'react-native-elements';
+import { FlatGrid } from 'react-native-super-grid';
 
 class WardrobeScreen extends React.Component {
-   constructor(props) {
-      super(props);
-      this.state = { selectedDate: new Date() };
-   }
+  constructor(props) {
+    super(props);
+    this.state = { dataSource: [] };
+  }
 
-   _onPressButton() {
-      Alert.alert('You tapped the button!')
-   }
+  componentDidMount() {
+    let items = Array.apply(null, Array(15)).map((v, i) => {
+      return { id: i, src: 'http://placehold.it/200x200?text=' + (i + 1) };
+    });
 
-   _onLongPressButton() {
-      Alert.alert('You long-pressed the button!')
-   }
+    this.setState({
+      dataSource: items,
+    });
+  }
 
-   render() {
+  renderItem(item, navigate, state) {
 
-      Title = <Text style={styles.title}>Wardrobe</Text>
-      CalendarButton = <Icon name='ios-calendar' type='ionicon' color='white' underlayColor='transparent'
-         onPress={() => this.props.navigation.navigate('Calendar')}
-         hitSlop={{left: 30, top: 10, bottom: 10}} />
+    Thumbnail =
+      <Image
+        style={styles.imageThumbnail}
+        source={{ uri: item.src }} />
 
-      return (
-         <View style={styles.container}>
+    MoreButton =
+      <Icon
+        name='ios-more'
+        type='ionicon'
+        color='black'
+        containerStyle={styles.menuIcon}
+        underlayColor='transparent' />
 
-         <Header
-            centerComponent={Title}
-            rightComponent={CalendarButton}/>
-         <TouchableHighlight
-            onPress={this._onPressButton}
-            onLongPress={this._onLongPressButton}>
-            <Text>Button</Text>
-         </TouchableHighlight>
+    Label = <Text style={styles.text}>Synchilla</Text>
 
-         </View>
-      );
-   }
+    return (
+      <View style={styles.box}>
+        <TouchableWithoutFeedback>
+          <View>
+            {Thumbnail}
+            {MoreButton}
+            {Label}
+
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+    );
+  }
+
+  render() {
+
+    Title = <Text style={styles.title}>Wardrobe</Text>
+
+    CalendarButton =
+      <Icon
+        name='ios-calendar'
+        type='ionicon'
+        color='white'
+        underlayColor='transparent'
+        onPress={() => this.props.navigation.navigate('Calendar')}
+        hitSlop={{left: 30, top: 10, bottom: 10}} />
+
+    return (
+      <View style={styles.container}>
+
+      <Header
+      centerComponent={Title}
+      rightComponent={CalendarButton}/>
+
+      <FlatGrid
+        items={this.state.dataSource}
+        itemDimension={130}
+        renderItem={({ item }) => (
+            this.renderItem(item, this.props.navigation, this.props.navigation)
+          )}
+        spacing={0}
+      />
+
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -48,9 +93,30 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    //justifyContent: 'center',
     alignItems: 'center'
+  },
+  box: {
+    backgroundColor: '#fff0b3',
+    borderColor: '#fff',
+    borderWidth: 5,
+    margin: 3,
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
+    fontFamily: "Optima",
+  },
+  imageThumbnail: {
+    //justifyContent: 'flex-end',
+    //alignItems: 'center',
+    height: 175,
+  },
+  menuIcon: {
+    position: 'absolute',
+    top: 0,
+    right: 10,
   }
-})
+});
 
 export default WardrobeScreen;
